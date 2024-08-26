7f697432a6d775aa469817ac73725f921a117eb2
jfdev <jfdev73@gmail.com>
2024-08-25 22:51:28 -0600

    first commit

---
+++ src/app/prime-ng/prime-ng.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { MenuModule } from 'primeng/menu';
+import { MenubarModule } from 'primeng/menubar';
+
+@NgModule({
+  declarations: [],
+  imports: [],
+  exports: [MenuModule, MenubarModule],
+})
+export class PrimeNgModule {}
---
+++ src/app/shared/components/menu/menu.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { Menu } from 'primeng/menu';
+
+@Component({
+  selector: 'shared-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css'],
+})
+export class MenuComponent {
+  menuItems: MenuItem[] = [];
+
+  ngOnInit(): void {
+    this.menuItems = [
+      {
+        label: 'Pipes de Angular',
+        icon: 'pi pi-desktop',
+        items: [
+          {
+            label: 'Textos y Fechas',
+            icon: 'pi pi-align-left',
+            routerLink: ' ',
+          },
+          { label: 'Numeros', icon: 'pi pi-dollar', routerLink: 'numbers' },
+          { label: 'No comunes', icon: 'pi pi-globe', routerLink: 'uncommon' },
+        ],
+      },
+      {
+        label: 'Pipes Personalizados',
+        icon: 'pi pi-cog',
+        items: [{ label: 'Otro elemento', icon: 'pi pi-cog' }],
+      },
+    ];
+  }
+}
