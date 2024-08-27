07134b0d0dea6d5f781464dcb6265d99c9f0ec81
jfdev <jfdev73@gmail.com>
2024-08-26 22:09:32 -0600

    Using  PrimeFlex and Testing Date pipe

--- src/app/products/pages/basics-page/basics-page.component.ts
+++ src/app/products/pages/basics-page/basics-page.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-basics-page',
   templateUrl: './basics-page.component.html',
-  styleUrls: ['./basics-page.component.css']
+  styleUrls: ['./basics-page.component.css'],
 })
 export class BasicsPageComponent {
+  nameLower: string = 'fernando';
+  nameUpper: string = 'FERNANDO';
+  nameComplete: string = 'feRnAnDo heRrera';
 
+  customDate: Date = new Date();
 }
