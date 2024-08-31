c547c840feced2243f17e4f76657961a22479a58
jfdev <jfdev73@gmail.com>
2024-08-30 19:34:05 -0600

    using uncommon pipes

--- src/app/products/pages/numbers-page/numbers-page.component.ts
+++ src/app/products/pages/numbers-page/numbers-page.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-numbers-page',
   templateUrl: './numbers-page.component.html',
-  styleUrls: ['./numbers-page.component.css']
+  styleUrls: ['./numbers-page.component.css'],
 })
 export class NumbersPageComponent {
-
+  totalSells: number = 2566789.5567;
+  percent: number = 0.4856;
 }
--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -1,10 +1,50 @@
 import { Component } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-uncommon-page',
   templateUrl: './uncommon-page.component.html',
-  styleUrls: ['./uncommon-page.component.css']
+  styleUrls: ['./uncommon-page.component.css'],
 })
 export class UncommonPageComponent {
+  //i18n Select
+  name: string = 'John';
 
+  gender: 'male' | 'female' = 'male';
+
+  invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla',
+  };
+
+  changeClient(): void {
+    this.name = 'Melisa';
+    this.gender = 'female';
+  }
+
+  //i18n Plural
+  clients: string[] = ['John', 'Peter', 'Mary', 'Lucy'];
+  clientsMap = {
+    '=0': 'no tenemos ningún cliente esperando',
+    '=1': 'tenemos un cliente esperando',
+    other: 'tenemos # clientes epserando',
+  };
+
+  deleteClient(): void {
+    this.clients.shift();
+  }
+
+  //keyvalue pipe
+  person = {
+    name: 'John',
+    age: 35,
+    address: 'New York',
+  };
+
+  noSorting = (a: any, b: any) => {
+    return 1;
+  };
+
+  //Async pipe
+  public myObservableTimer = interval(2000);
 }
